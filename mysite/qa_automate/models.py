cace9789bdfa96af06c988065fdde2bbb82d6383
heeseopark <heeseopark@korea.ac.kr>
2023-03-07 19:31:20 +0900

    0307 commit

--- mysite/qa_automate/models.py
+++ mysite/qa_automate/models.py
@@ -3,15 +3,27 @@ from datetime import date
 
 # Create your models here.
 
-class DateCheck(models.Model):
+class DateCheckTest(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
     date = models.DateField()
-    is_checked = models.BooleanField(default=False)
 
-class Question(models.Model):
-    page_number = models.PositiveIntegerField(default=1)
-    question_number = models.PositiveIntegerField(default=1)
-    count = models.PositiveIntegerField(default=1)
-    date = models.DateField()
+class BookTest(models.Model):
+    title = models.CharField(max_length=200, primary_key=True)
+
+class BookQuestionTest(models.Model):
+    question_id = models.IntegerField(primary_key=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    question_number = models.IntegerField()
+    student_name = models.CharField(max_length=200)
+    student_id = models.CharField(max_length=20)
+    page = models.IntegerField()
+    number = models.IntegerField()
+    theme = models.IntegerField()
+    example = models.IntegerField()
+
+class BlacklistTest(models.Model):
+    student_name = models.CharField(max_length=200)
+    student_id = models.CharField(max_length=20)
 
 class SibalSooSangDate(DateCheck):
     pass
--- mysite/qa_automate/views.py
+++ mysite/qa_automate/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-
-
+from django.http import HttpResponseRedirect
+from .models import BookTest
+from .models import BlacklistTest
 # Create your views here.
 
 
@@ -12,7 +13,111 @@ def init(request):
 def calender(request):
     return render(request, 'qa_automate/datepicker.html')
 
+def book_list(request):
+    books = BookTest.objects.all()
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        book = BookTest(title=title)
+        book.save()
+        return HttpResponseRedirect('/')
+
+    return render(request, 'book_list.html', {'books': books})
+
+def blacklist(request):
+    elements = BlacklistTest.objects.all()
+
+    if request.method == 'POST':
+        student_name = request.POST.get('student_name')
+        student_id = request.POST.get('student_id')
+        element = BlacklistTest(student_name=student_name, student_id=student_id)
+        element.save()
+        return HttpResponseRedirect('/blacklist/')
+
+    return render(request, 'blacklist.html', {'elements': elements})
+
+import time
+from selenium import webdriver
+
+def goToWaitingPage():
+    # set up the Chrome driver
+    driver = webdriver.Chrome()
+
+    # navigate to the website
+    driver.get('https://www.example.com/')
+
+    # find and click the first element
+    first_element = driver.find_element_by_xpath('/html/body/form/div/div[1]/label[2]/input')
+    first_element.click()
+
+    # find and input text into the second element
+    second_element = driver.find_element_by_id('id')
+    second_element.send_keys('heeseopark')
+
+    # find and click the third element
+    third_element = driver.find_element_by_id('sp_smsct')
+    third_element.click()
+
+    # find and input text into the fourth element
+    fourth_element = driver.find_element_by_id('second_tec_cd')
+    fourth_element.send_keys('heeseo1099')
+
+    # find and click the fifth element
+    fifth_element = driver.find_element_by_id('sp_login')
+    fifth_element.click()
+
+    # wait for 3 seconds
+    time.sleep(3)
+
+    # find and click the sixth element
+    sixth_element = driver.find_element_by_id('a594')
+    sixth_element.click()
+
+    # wait for 3 seconds
+    time.sleep(3)
+
+    # find and click the seventh element
+    seventh_element = driver.find_element_by_id('aa4233')
+    seventh_element.click()
 
 
-#/
+def goToTotalPage():
+    # set up the Chrome driver
+    driver = webdriver.Chrome()
 
+    # navigate to the website
+    driver.get('https://www.example.com/')
+
+    # find and click the first element
+    first_element = driver.find_element_by_xpath('/html/body/form/div/div[1]/label[2]/input')
+    first_element.click()
+
+    # find and input text into the second element
+    second_element = driver.find_element_by_id('id')
+    second_element.send_keys('heeseopark')
+
+    # find and click the third element
+    third_element = driver.find_element_by_id('sp_smsct')
+    third_element.click()
+
+    # find and input text into the fourth element
+    fourth_element = driver.find_element_by_id('second_tec_cd')
+    fourth_element.send_keys('heeseo1099')
+
+    # find and click the fifth element
+    fifth_element = driver.find_element_by_id('sp_login')
+    fifth_element.click()
+
+    # wait for 3 seconds
+    time.sleep(3)
+
+    # find and click the sixth element
+    sixth_element = driver.find_element_by_id('a594')
+    sixth_element.click()
+
+    # wait for 3 seconds
+    time.sleep(3)
+
+    # find and click the seventh element
+    seventh_element = driver.find_element_by_id('aa4231')
+    seventh_element.click()
