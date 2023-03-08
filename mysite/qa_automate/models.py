b4b6c1820010f4afe615483b653c6bffe0482db6
heeseopark <heeseopark@korea.ac.kr>
2023-03-08 13:47:27 +0900

    0308 commit

--- mysite/qa_automate/models.py
+++ mysite/qa_automate/models.py
@@ -3,16 +3,16 @@ from datetime import date
 
 # Create your models here.
 
-class DateCheckTest(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    date = models.DateField()
-
 class BookTest(models.Model):
     title = models.CharField(max_length=200, primary_key=True)
 
+class DateCheckTest(models.Model):
+    book = models.ForeignKey(BookTest, on_delete=models.CASCADE)
+    date = models.DateField()
+
 class BookQuestionTest(models.Model):
     question_id = models.IntegerField(primary_key=True)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    book = models.ForeignKey(BookTest, on_delete=models.CASCADE)
     question_number = models.IntegerField()
     student_name = models.CharField(max_length=200)
     student_id = models.CharField(max_length=20)
@@ -25,9 +25,4 @@ class BlacklistTest(models.Model):
     student_name = models.CharField(max_length=200)
     student_id = models.CharField(max_length=20)
 
-class SibalSooSangDate(DateCheck):
-    pass
-
-class SibalSooSangQuestion(Question):
-    pass
-
+    
