53b296d4565702d083f3f022adeb4d2e1f82f7e6
heeseopark <heeseopark@korea.ac.kr>
2023-03-15 11:29:06 +0900

    0315 commit

--- mysite/qa_automate/functions.py
+++ mysite/qa_automate/functions.py
@@ -13,83 +13,80 @@ def isInBlackList(text):
     except BlacklistTest.DoesNotExist:
         return False
     
-
-
-def goToWaitingPage(driver):
+def goToWaitingPage(browser):
     # navigate to the website
-    driver.get('https://www.tzonemegastudy.net/')
+    browser.get('https://www.tzonemegastudy.net/')
 
     # find and click the first element
-    first_element = driver.find_element_by_xpath('/html/body/form/div/div[1]/label[2]/input')
+    first_element = browser.find_element_by_xpath('/html/body/form/div/div[1]/label[2]/input')
     first_element.click()
 
     # find and input text into the second element
-    second_element = driver.find_element_by_id('id')
+    second_element = browser.find_element_by_id('id')
     second_element.send_keys('heeseopark')
 
     # find and click the third element
-    third_element = driver.find_element_by_id('sp_smsct')
+    third_element = browser.find_element_by_id('sp_smsct')
     third_element.click()
 
     # find and input text into the fourth element
-    fourth_element = driver.find_element_by_id('second_tec_cd')
+    fourth_element = browser.find_element_by_id('second_tec_cd')
     fourth_element.send_keys('heeseo1099')
 
     # find and click the fifth element
-    fifth_element = driver.find_element_by_id('sp_login')
+    fifth_element = browser.find_element_by_id('sp_login')
     fifth_element.click()
 
     # wait for 3 seconds
     time.sleep(3)
 
     # find and click the sixth element
-    sixth_element = driver.find_element_by_id('a594')
+    sixth_element = browser.find_element_by_id('a594')
     sixth_element.click()
 
     # wait for 3 seconds
     time.sleep(3)
 
     # find and click the seventh element
-    seventh_element = driver.find_element_by_id('aa4233')
+    seventh_element = browser.find_element_by_id('aa4233')
     seventh_element.click()
 
-
-def goToTotalPage(driver):
+def goToTotalPage(browser):
     # navigate to the website
-    driver.get('https://www.example.com/')
+    browser.get('https://www.example.com/')
 
     # find and click the first element
-    first_element = driver.find_element_by_xpath('/html/body/form/div/div[1]/label[2]/input')
+    first_element = browser.find_element_by_xpath('/html/body/form/div/div[1]/label[2]/input')
     first_element.click()
 
     # find and input text into the second element
-    second_element = driver.find_element_by_id('id')
+    second_element = browser.find_element_by_id('id')
     second_element.send_keys('heeseopark')
 
     # find and click the third element
-    third_element = driver.find_element_by_id('sp_smsct')
+    third_element = browser.find_element_by_id('sp_smsct')
     third_element.click()
 
     # find and input text into the fourth element
-    fourth_element = driver.find_element_by_id('second_tec_cd')
+    fourth_element = browser.find_element_by_id('second_tec_cd')
     fourth_element.send_keys('heeseo1099')
 
     # find and click the fifth element
-    fifth_element = driver.find_element_by_id('sp_login')
+    fifth_element = browser.find_element_by_id('sp_login')
     fifth_element.click()
 
     # wait for 3 seconds
     time.sleep(3)
 
     # find and click the sixth element
-    sixth_element = driver.find_element_by_id('a594')
+    sixth_element = browser.find_element_by_id('a594')
     sixth_element.click()
 
     # wait for 3 seconds
     time.sleep(3)
 
     # find and click the seventh element
-    seventh_element = driver.find_element_by_id('aa4231')
+    seventh_element = browser.find_element_by_id('aa4231')
     seventh_element.click()
 
 def paging(browser):
