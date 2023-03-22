e7958dfc8f2b51a81a434e73d08329e6ebc5abcd
heeseopark <heeseopark@korea.ac.kr>
2023-03-22 20:00:21 +0900

    0322 commit

--- mysite/qa_automate/urls.py
+++ mysite/qa_automate/urls.py
@@ -5,7 +5,7 @@ app_name = 'qa_automate'
 
 urlpatterns = [
     path('', views.init, name='init'),
-    path('calender/<str:book_title', views.calender),
+    path('calender/<str:book_title>', views.calender, name='calender'),
     path('booklist/', views.booklist),
     path('blacklist/', views.blacklist),
     path('search/', views.search_date),
